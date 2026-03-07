e5ed76dd501bfcadb4dbc9e51932cc5e6cde3a45
Odilbek <=>
2026-03-07 13:06:34 +0500

    c

--- filters/user.py
+++ filters/user.py
@@ -15,8 +15,19 @@ class IsRegisteredUser(BaseFilter):
 
 class InTeachersResumeState(BaseFilter):
     async def __call__(self, message: Message, state: FSMContext) -> bool:
-        return await state.get_state() in [TeachersVacancyState.subject, TeachersVacancyState.working_time, TeachersVacancyState.has_sertificate, TeachersVacancyState.sertificate_name, TeachersVacancyState.sertificate_ball, TeachersVacancyState.sertificate_file, TeachersVacancyState.experience, TeachersVacancyState.last_work_place, TeachersVacancyState.why_leave_work, TeachersVacancyState.last_work_place_phone, TeachersVacancyState.salary, TeachersVacancyState.position, TeachersVacancyState.why_choice_us]
+        return await state.get_state() in [
+            TeachersVacancyState.subject, TeachersVacancyState.working_time, TeachersVacancyState.has_sertificate, 
+            TeachersVacancyState.sertificate_name, TeachersVacancyState.sertificate_ball, TeachersVacancyState.sertificate_file, 
+            TeachersVacancyState.experience, TeachersVacancyState.last_work_place, TeachersVacancyState.why_leave_work, 
+            TeachersVacancyState.last_work_place_phone, TeachersVacancyState.salary, TeachersVacancyState.position, 
+            TeachersVacancyState.why_choice_us, TeachersVacancyState.confirm, TeachersVacancyState.are_you_student, TeachersVacancyState.university
+        ]
 
 class InAdminsResumeState(BaseFilter):
     async def __call__(self, message: Message, state: FSMContext) -> bool:
-        return await state.get_state() in [AdminsVacancyState.vacancy_type, AdminsVacancyState.working_time, AdminsVacancyState.foreign_language, AdminsVacancyState.foreign_language_level, AdminsVacancyState.experience, AdminsVacancyState.last_work_place, AdminsVacancyState.why_leave_work, AdminsVacancyState.last_work_place_phone, AdminsVacancyState.why_choice_us]
+        return await state.get_state() in [
+            AdminsVacancyState.vacancy_type, AdminsVacancyState.working_time, AdminsVacancyState.foreign_language, 
+            AdminsVacancyState.foreign_language_level, AdminsVacancyState.experience, AdminsVacancyState.last_work_place, 
+            AdminsVacancyState.why_leave_work, AdminsVacancyState.last_work_place_phone, AdminsVacancyState.why_choice_us,
+            AdminsVacancyState.confirm
+        ]
